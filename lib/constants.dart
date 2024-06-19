77cfab598d576c3a3dfe94c3856630d95f2c8cf4
Vikranth Reddy <vikranthr@goextramile.in>
2024-06-19 12:20:46 +0530

    updated code

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 const TextStyle cvRegularStyle=TextStyle(fontFamily: "Bebas", fontSize: 40, fontWeight: FontWeight.bold);
 const TextStyle cvTitleStyle=TextStyle(fontFamily: "Bebas", fontSize: 20, fontWeight: FontWeight.bold);
+const TextStyle cvCardStyle=TextStyle(
+fontFamily: "Bebas", fontSize: 20);
--- lib/screens/my_cv_page.dart
+++ lib/screens/my_cv_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:my_cv_app/constants.dart';
 //import 'package:flutter/widgets.dart';
@@ -20,9 +22,64 @@ class MyCVPage extends StatelessWidget {
               foregroundImage: AssetImage("assets/flower.jpeg"),
             ),
             Text("Vikranth Reddy", style: cvRegularStyle),
+            SizedBox(height: 8),
             Text("Fullstack Developer", style: cvTitleStyle),
-            Text("+91-9700636433", style: cvTitleStyle),
-            Text("ch.vikranth521@gmail.com", style: cvTitleStyle),
+            Padding(
+              padding:EdgeInsets.symmetric(horizontal: 25),
+              child: Card(
+                margin: EdgeInsets.all(8),
+                child: ListTile(
+                  leading: Icon(Icons.call,color: Colors.blueGrey,size: 40,),
+                  title: Center(child: Text("+91-9700636433",style: cvCardStyle)),
+                  
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 25),
+              child: Card(
+                child: ListTile(
+                  leading: Icon(Icons.email,color: Colors.blueGrey,size: 40,),
+                  title: Center(
+                    child: Text("ch.vikranth521@gmail.com",
+                    style: cvCardStyle)
+                    ),
+                  trailing: SizedBox(),
+                ),
+              ),
+            ),
+            
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    //flex: 2,
+                    child: Column(
+                      children: [
+                        Text("Education", textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),),
+                        SizedBox(height: 3),
+                        Text("B.Tech, CSE", textAlign: TextAlign.center),
+                        Text("JNTU,Hyd", textAlign: TextAlign.center)
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    //flex: 6,
+                    child: Column(
+                      children: [
+                        Text("Work experience", textAlign: TextAlign.center,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),),
+                        SizedBox(height: 3),
+                        Text("FullStack Developer", textAlign: TextAlign.center),
+                        Text(".Net  Developer", textAlign: TextAlign.center)
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       )),
